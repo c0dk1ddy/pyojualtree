f63bb6d4c2f59e95e1e42308b383e855280eb3d2
c0dk1ddy <tax0787bz3@gmail.com>
2025-04-02 13:40:22 +0900

    phhhhhhhhshit

--- include/constexprcompiling.h
+++ include/constexprcompiling.h
@@ -3,20 +3,27 @@
 #define _CONSTEXPRCOMPILING_H
 #include <pyojualtree_cpp_wrapper.h>
 
-template <typename T, T L>
-struct __v_t {
+template <typename T, T L, T I = L - 1>
+constexpr struct __v_t {
 	noncompiled_block core[L];
+	constexpr inline blocks operator&(void) {
+		return &this->core[I];
+	}
 };
 
 template <typename T, T L, T B = (3*L)+1, typename G = __v_t<T, L>>
-struct constexprv_t {
+constexpr struct constexprv_t {
 	union {
 		char s[B];
 		G v;
 	};
-	using v_t = G; 
-	
+	using v_t = G;
 };
 
-constexpr inline 
+template <typename T, T L, typename G = constexpr_t<T, L>>
+constexpr static inline auto constexprcompiling(G x) {
+	if constexpr (compile(&x.v.core, &x.v)) {
+	
+	}
+}
 #endif
