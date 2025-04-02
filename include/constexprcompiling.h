cdc37fd99e0ee332cfb78c7abf7051849acbb162
c0dk1ddy <tax0787bz3@gmail.com>
2025-04-02 11:09:01 +0900

    need2c0d

---
+++ include/constexprcompiling.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef _CONSTEXPRCOMPILING_H
+#define _CONSTEXPRCOMPILING_H
+#include <pyojualtree_cpp_wrapper.h>
+#endif
---
+++ include/pyojualtree_cpp_wrapper.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+
+#ifndef _PYOJUALTREE_CPP_WRAPPER_H
+#define _PYOJUALTREE_CPP_WRAPPER_H
+#endif
+
+#ifndef _cplusplus
+}
+#endif
